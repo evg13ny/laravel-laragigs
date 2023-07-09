2913dac99054975bd93ea1013343f2652e2c312a
Evgeny Polyakov <evg_pol@yahoo.com>
2023-07-09 16:35:01 +0300

    added create new user function

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -31,10 +31,12 @@
                 alt="" class="logo" /></a>
         <ul class="flex space-x-6 mr-6 text-lg">
             <li>
-                <a href="register.html" class="hover:text-laravel"><i class="fa-solid fa-user-plus"></i> Register</a>
+                <a href="http://localhost/laravel-laragigs/public/register" class="hover:text-laravel"><i
+                        class="fa-solid fa-user-plus"></i> Register</a>
             </li>
             <li>
-                <a href="login.html" class="hover:text-laravel"><i class="fa-solid fa-arrow-right-to-bracket"></i>
+                <a href="http://localhost/laravel-laragigs/public/login" class="hover:text-laravel"><i
+                        class="fa-solid fa-arrow-right-to-bracket"></i>
                     Login</a>
             </li>
         </ul>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
-use App\Http\Controllers\ListingController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\ListingController;
 
 /*
 |--------------------------------------------------------------------------
@@ -32,6 +33,11 @@ Route::put('/listings/{listing}', [ListingController::class, 'update']);
 // Delete Listing
 Route::delete('/listings/{listing}', [ListingController::class, 'destroy']);
 
-
 // Single Listing
 Route::get('/listings/{listing}', [ListingController::class, 'show']);
+
+// Show Register/Create Form
+Route::get('/register', [UserController::class, 'create']);
+
+// Create New User
+Route::post('/users', [UserController::class, 'store']);
